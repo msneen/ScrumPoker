99f88c14bfdbaa3806c7e71aed140ae878beaabe
msneen <michael_sneen@yahoo.com>
2013-01-05 01:08:17 -0800

    Basic page working.

--- ScrumPoker/Controllers/HomeController.cs
+++ ScrumPoker/Controllers/HomeController.cs
@@ -3,14 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ScrumPoker.Models;
 
 namespace ScrumPoker.Controllers
 {
     public class HomeController : Controller
     {
 
-        public ActionResult Poker()
+        
+
+        public ActionResult Poker(string estimate, string firstname)
         {
+            List<string> colors = new List<string>() { "Red", "Green", "Cyan", "Chartreuse", "Coral" };
+
+            if (!string.IsNullOrEmpty(estimate))
+            {
+                //Session["TaskEstimates"] = null;
+                List<TaskEstimate> taskEstimates = Session["TaskEstimates"] as List<TaskEstimate>;
+                if (taskEstimates == null)
+                {
+                    taskEstimates = new List<TaskEstimate>();
+                    Session["TaskEstimates"] = taskEstimates;
+                }
+
+                TaskEstimate currentEstimate = (from e in taskEstimates
+                                                where  firstname.Equals(e.Name, StringComparison.OrdinalIgnoreCase)
+                                                //&& estimate.Equals(e.Estimate, StringComparison.OrdinalIgnoreCase)
+                                               select e).FirstOrDefault<TaskEstimate>();
+
+                if (currentEstimate != null)
+                {
+                    currentEstimate.Estimate = estimate;
+                }
+                else
+                {
+                    taskEstimates.Add(new TaskEstimate() { Name = firstname, Estimate = estimate });
+                }
+
+                ViewBag.Estimate = estimate;
+                ViewBag.FirstName = firstname;
+                ViewBag.Estimates = taskEstimates;
+                ViewBag.Colors = colors;
+            }
+
             return View();
         }
 
