9aa105c3efaa9b5ce31c3d772fe1501b75ecab37
msneen <michael_sneen@yahoo.com>
2013-01-06 23:55:57 -0800

    Made SiteAdminSvc and IsSiteAdmin Method for adding Roles and other db setup values

---
+++ ScrumPoker/Services/SiteAdminSvc.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ScrumPoker.Services
+{
+    public class SiteAdminSvc
+    {
+        public static bool IsSiteAdmin()
+        {
+            //this is just temporary.  Should be determined by membership classes
+            return ScrumMasterSvc.IsScrumMaster();
+        }
+    }
+}
