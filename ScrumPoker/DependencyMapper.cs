3fbd3300c4b4ce0c3ac63f56ad3ddb312742def5
msneen <michael_sneen@yahoo.com>
2013-01-20 21:19:36 -0800

    First Compile and successful run with Ninject

---
+++ ScrumPoker/DependencyMapper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Ninject.Modules;
+
+namespace ScrumPoker
+{
+    public class DependencyMapper : NinjectModule
+    {
+        public override void Load()
+        {
+            this.Bind<Services.UserProfileSvc>().To<Services.UserProfileSvc>();
+        }
+    }
+}
--- ScrumPoker/Global.asax.cs
+++ ScrumPoker/Global.asax.cs
@@ -6,16 +6,31 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Ninject;
+using System.Reflection;
+using Ninject.Web.Common;
 
 namespace ScrumPoker
 {
     // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
     // visit http://go.microsoft.com/?LinkId=9394801
 
-    public class MvcApplication : System.Web.HttpApplication
+    public class MvcApplication : Ninject.Web.Common.NinjectHttpApplication //System.Web.HttpApplication
     {
-        protected void Application_Start()
+        protected override IKernel CreateKernel()
         {
+            var kernel = new StandardKernel();
+            kernel.Load(Assembly.GetExecutingAssembly());
+            kernel.Bind<Func<IKernel>>().ToMethod(ctx => () => new Bootstrapper().Kernel);
+            kernel.Bind<IHttpModule>().To<HttpApplicationInitializationHttpModule>();
+            return kernel;
+        }
+
+        //protected void Application_Start()
+        protected override void  OnApplicationStarted()
+        {
+ 	        base.OnApplicationStarted();
+       
             AreaRegistration.RegisterAllAreas();
 
             WebApiConfig.Register(GlobalConfiguration.Configuration);
